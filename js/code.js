582e5ff19ab2516d6e8d8df8022f4ce2679131f3
Brogy-alt <brogangys2@gmail.com>
2022-11-07 13:23:25 +0200

    push

---
+++ js/code.js
@@ -0,0 +1,12 @@
+// function madeby() {
+//     alert("Made By Thabo mbeki");
+
+// }
+
+let madeby = document.querySelector("button");
+// Add listener//
+madeby.addEventListener('click',()=>{
+    alert("Made By Thabo mbeki");
+
+});
+// add eventlistener seperates (it is the perfect/right way to do this)
